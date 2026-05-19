06874975e78fe3ea9810626d3386a1a39307e4a0
Nick Wilson <logikfx@gmail.com>
2026-05-19 16:43:05 -0400

    Updating code and tests for French check.

--- hello.go
+++ hello.go
@@ -3,13 +3,18 @@ package learn_go_with_tests
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 	if language == "Spanish" {
-		return "Hola, " + name
+		return spanishHelloPrefix + name
+	}
+	if language == "French" {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
