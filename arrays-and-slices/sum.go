760ab65107dcf5d80efa49a99a96ef5e4667c9ed
Nick Wilson <logikfx@gmail.com>
2026-05-20 17:53:38 -0400

    Added test and refactored function to be able to take a variable size array.

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,10 +1,7 @@
 package arrays_and_slices
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	/*for i := 0; i < 5; i++ {
-		sum += numbers[i]
-	}*/
 	for _, number := range numbers {
 		sum += number
 	}
