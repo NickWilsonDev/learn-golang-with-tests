fbf9596e5dfd7c84027a582c715c5575a26573dd
Nick Wilson <logikfx@gmail.com>
2026-05-21 16:00:22 -0400

    Refactored tests and functions to work with triangles.

--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -12,7 +12,7 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
-func TestArea(t *testing.T) {
+/*func TestArea(t *testing.T) {
 
 	checkArea := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
@@ -32,4 +32,26 @@ func TestArea(t *testing.T) {
 		checkArea(t, circle, 314.1592653589793)
 	})
 
+} */
+
+func TestArea(t *testing.T) {
+
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("got %g want %g", got, tt.hasArea)
+			}
+		})
+	}
 }
