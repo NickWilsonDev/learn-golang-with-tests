589fdba18c8d084c1c842779a20caa89d09bda4b
Nick Wilson <logikfx@gmail.com>
2026-05-22 14:36:50 -0400

    Refactor involving Stringer and Bitcoin.

--- pointers-and-errors/wallet.go
+++ pointers-and-errors/wallet.go
@@ -2,15 +2,25 @@ package pointers_and_errors
 
 import "fmt"
 
-type Wallet struct {
-	balance int
+type Stringer interface {
+	String() string
 }
 
-func (w *Wallet) Deposit(amount int) {
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
